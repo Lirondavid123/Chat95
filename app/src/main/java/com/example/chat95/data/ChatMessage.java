0160e1237b58f447d566f6fd07c1b7d895ef7a5b
Lirondavid123 <lirondavid.work@gmail.com>
2020-05-26 23:10:55 +0300

    26/5/20 הוספתי את השם של כולם

--- app/src/main/java/com/example/chat95/data/ChatMessage.java
+++ app/src/main/java/com/example/chat95/data/ChatMessage.java
@@ -16,7 +16,7 @@ public class ChatMessage {
         this.receiverId = receiverId;
         this.timeStamp = timeStamp;
     }
-//liron
+//liron-naor-oriel-or
     public String getTextMessage() {
         return textMessage;
     }
